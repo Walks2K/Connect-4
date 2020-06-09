50cacbaf472279a8f2f008f968177ce0265bd5b8
Walks2K <walks2000@gmail.com>
2020-06-09 13:37:44 +0100

    Functional game (needs visual polish), will work on minimax function soon (evaluate board + algorithm required). Also added combo box to change game mode, need to add implementation of that.

---
+++ GameTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_4
+{
+    /// <summary>
+    /// Enumerator for game types
+    /// </summary>
+    public enum GameTypes
+    {
+        PlayerVersusPlayer,
+        PlayerVersusAI,
+        AIVersusAI
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,6 +26,11 @@ namespace Connect_4
         /// True if the game has ended
         /// </summary>
         private bool mGameEnded;
+
+        /// <summary>
+        /// Holds the information about which game is being played (PVP or PVE etc)
+        /// </summary>
+        private GameTypes mGameType = GameTypes.PlayerVersusPlayer;
         #endregion
         #region Constructor
         public MainWindow()
@@ -98,16 +103,109 @@ namespace Connect_4
                     mPlayer1Turn = !mPlayer1Turn;
 
                     // Check for a winner
-                    CheckForWinner();
+                    var Winner = CheckForWinner(mCellTypes);
+
+                    if (Winner == CellTypes.Red)
+                    {
+                        mGameEnded = true;
+                    }
+                    else if (Winner == CellTypes.Yellow)
+                    {
+                        mGameEnded = true;
+                    }
 
                     return;
                 }
             }
         }
 
-        private void CheckForWinner()
+        private CellTypes CheckForWinner(CellTypes[,] board)
         {
-            
+            // Horizontal check
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1) - 3; j++)
+                {
+                    var player = board[i, j];
+                    if (player == CellTypes.Free)
+                        continue;
+
+                    if (board[i, j] == player && board[i, j + 1] == player && board[i, j + 2] == player && board[i, j + 3] == player)
+                        return player;
+                }
+            }
+
+            // Vertical check
+            for (int i = 0; i < board.GetLength(0) - 3; i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    var player = board[i, j];
+                    if (player == CellTypes.Free)
+                        continue;
+
+                    if (board[i, j] == player && board[i + 1, j] == player && board[i + 2, j] == player && board[i + 3, j] == player)
+                        return player;
+                }
+            }
+
+            // Ascending diagonal check
+            for (int i = 3; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1) - 3; j++)
+                {
+                    var player = board[i, j];
+                    if (player == CellTypes.Free)
+                        continue;
+
+                    if (board[i, j] == player && board[i - 1, j + 1] == player && board[i - 2, j + 2] == player && board[i - 3, j + 3] == player)
+                        return player;
+                }
+            }
+
+            // Descending diagonal check
+            for (int i = 3; i < board.GetLength(0); i++)
+            {
+                for (int j = 3; j < board.GetLength(1); j++)
+                {
+                    var player = board[i, j];
+                    if (player == CellTypes.Free)
+                        continue;
+
+                    if (board[i, j] == player && board[i - 1, j - 1] == player && board[i - 2, j - 2] == player && board[i - 3, j - 3] == player)
+                        return player;
+                }
+            }
+
+            // Check for no winner and full board
+            if (!board.Cast<CellTypes>().Any(result => result == CellTypes.Free))
+            {
+                // Game ended
+                mGameEnded = true;
+
+                // Turn all cells orange
+                Container.Children.Cast<Button>().ToList().ForEach(button =>
+                {
+                    button.Background = Brushes.Orange;
+                });
+            }
+
+            return CellTypes.Free;
+        }
+
+        /// <summary>
+        /// Update selection on combo box update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Cast sender to combo box
+            var combobox = (ComboBox)sender;
+            // Update game type to match selection
+            mGameType = (GameTypes)combobox.SelectedIndex;
+            // Start a new game
+            NewGame();
         }
     }
     #endregion
